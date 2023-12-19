d597d5c022f1b45dbe8fb288da681f2e32cd1530
tsssni <dingyongyu2002@foxmail.com>
2023-12-19 22:16:18 +0800

    first commit

---
+++ lua/lualine/themes/tsssni-theme.lua
@@ -0,0 +1,37 @@
+return {
+  normal = {
+    a = { fg = "#f8f8ff", bg = "#ff0055" },
+    b = { fg = "#f5c1e9", bg = "#333333" },
+    c = { fg = "#f5c1e9", bg = "#000000" },
+  },
+
+  insert = {
+    a = { fg = "#f8f8ff", bg = "#61a702" },
+    b = { fg = "#f5c1e9", bg = "#333333" },
+    c = { fg = "#f5c1e9", bg = "#000000" },
+  },
+
+  visual = {
+    a = { fg = "#f8f8ff", bg = "#4685ea" },
+    b = { fg = "#f5c1e9", bg = "#333333" },
+    c = { fg = "#f5c1e9", bg = "#000000" },
+  },
+
+  command = {
+    a = { fg = "#f8f8ff", bg = "#8b4efc" },
+    b = { fg = "#f5c1e9", bg = "#333333" },
+    c = { fg = "#f5c1e9", bg = "#000000" },
+  },
+
+  replace = {
+    a = { fg = "#f8f8ff", bg = "#777777" },
+    b = { fg = "#f5c1e9", bg = "#333333" },
+    c = { fg = "#f5c1e9", bg = "#000000" },
+  },
+
+  inactive = {
+    a = { fg = "#f8f8ff", bg = "#333333" },
+    b = { fg = "#f5c1e9", bg = "#333333" },
+    c = { fg = "#f5c1e9", bg = "#000000" },
+  },
+}
