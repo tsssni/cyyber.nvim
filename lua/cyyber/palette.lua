b5006ce11dd6b280df2a93c8b0c3cec02e569da6
tsssni <dingyongyu2002@foxmail.com>
2025-03-09 13:02:02 +0800

    make lualine theme back to avoid patching themes

---
+++ lua/cyyber/palette.lua
@@ -0,0 +1,30 @@
+local palette = {
+	front = '#f5c1e9',
+	back = '#120b10',
+
+	black = '#333333',
+	light_black = '#777777',
+
+	red = '#ff0055',
+	light_red = '#ff6699',
+
+	green = '#61a702',
+	light_green = '#bef743',
+
+	yellow = '#fccb2e',
+	light_yellow = '#ffff40',
+
+	blue = '#4685ea',
+	light_blue = '#85b4ff',
+
+	magenta = '#8b4efc',
+	light_magenta = '#a8a8ff',
+
+	cyan = '#00ffc8',
+	light_cyan = '#7ff5f5',
+
+	white = '#aaaabb',
+	light_white = '#f8f8ff',
+}
+
+return palette
